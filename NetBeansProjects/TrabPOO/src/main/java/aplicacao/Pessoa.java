7ee0bb983790843ed9ff6bb98be030b4b0cac98e
Bruno Cipriani <bcipriani@outlook.com>
2020-12-07 21:52:02 -0200

    Primeiro commit.

---
+++ NetBeansProjects/TrabPOO/src/main/java/aplicacao/Pessoa.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacao;
+
+/**
+ *
+ * @author bcipr
+ */
+public class Pessoa {
+    
+    private String nome;
+    
+}
